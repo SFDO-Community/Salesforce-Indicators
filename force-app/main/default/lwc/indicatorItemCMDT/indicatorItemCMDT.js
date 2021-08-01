09a06e312b834051a7ca704509d5b100ef24d8af
Jodie Miners <jminers@gmail.com>
2021-08-01 23:17:55 +1000

    First Try

---
+++ force-app/main/default/lwc/indicatorItemCMDT/indicatorItemCMDT.js
@@ -0,0 +1,15 @@
+import { LightningElement, api, wire } from 'lwc';
+import showIndicators from '@salesforce/apex/IndicatorsController.showIndicators';
+
+const columns = [
+    { label: 'Field Name', fieldName: 'fieldName', type: 'text' },
+    { label: 'Icon Name', fieldName: 'iconName', type: 'text' },
+    { label: 'Text Value', fieldName: 'textValue', type: 'text' },
+    { label: 'Image URL', fieldName: 'imgURL', type: 'text' },
+    { label: 'Hover Text', fieldName: 'hoverText', type: 'text' }
+];
+
+export default class IndicatorItemCMDT extends LightningElement {
+    @api
+    recordId;
+}
