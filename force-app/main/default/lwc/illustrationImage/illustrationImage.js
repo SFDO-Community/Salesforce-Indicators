932a3bc5d1f43ed2ca40d3ede683880374f418bd
tschug <timschug@gmail.com>
2022-11-12 01:03:44 -0500

    Illustrations LWC

---
+++ force-app/main/default/lwc/illustrationImage/illustrationImage.js
@@ -0,0 +1,29 @@
+import { LightningElement, api } from "lwc";
+import { default as images, empty } from "./images/index";
+
+export { images };
+export default class illustrationImage extends LightningElement {
+  /**
+   * The identifier for the image to show, in the format
+   * `[category]:[description]`. See
+   * https://www.lightningdesignsystem.com/components/illustration/ for what
+   * each option renders.
+   *
+   * @type {keyof images}
+   */
+  @api imageName;
+
+  render() {
+    const { imageName } = this;
+    if (images[imageName]) {
+      return images[imageName];
+    } else {
+      console.warn(
+        `Missing template for image name "${imageName}". Valid options are: ${Object.keys(
+          images
+        )}`
+      );
+      return empty;
+    }
+  }
+}
